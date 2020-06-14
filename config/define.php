d9c0ddd12f049d7c963c6b9db0aa5662a695c4c5
KrShivanshu <kumarshivanshu13@gmail.com>
2020-06-15 04:15:50 +0530

    database credentials and procfile added

--- config/define.php
+++ config/define.php
@@ -6,8 +6,9 @@ define('Email','1st.goosebumps@gmail.com');
 define('Password','iambump@13');
 
 //for connection making page
-define('db_host','localhost');
-define('db_user','root');
-define('db_pass','123456');
-define('db_name','ovs');
+define('db_host','remotemysql.com');
+define('db_user','9z6PkHnmn8');
+define('db_pass','2oqUJHrg84');
+define('db_name','9z6PkHnmn8');
+define('db_port','3306');
 ?>
